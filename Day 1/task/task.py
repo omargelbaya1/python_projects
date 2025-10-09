d09bee7675c379d6136f730faa7837fc1428ce16
Omar Gelbaya <omargelbaya1@gmail.com>
2025-10-09 22:08:35 +0100

    day 1 completed!

--- Day 1/task/task.py
+++ Day 1/task/task.py
@@ -1,1 +1,4 @@
-# Write your code below this line 👇
+print("Hello welcome to the jungle")
+city = input("Which City did you grow up in? \n")
+pet =  input("What is the name of your pet? \n")
+print("So cool the name of your band is " + city + " "+ pet)
