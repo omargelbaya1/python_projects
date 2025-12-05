1407c53310b65ace1a35c34db746c6a09570d784
Omar Gelbaya <omargelbaya1@gmail.com>
2025-12-05 11:35:02 +0000

    before the blackjack game

--- Day 10/Calculator Project/main.py
+++ Day 10/Calculator Project/main.py
@@ -1,4 +1,38 @@
 def add(n1, n2):
     return n1 + n2
 
+def subtract(n1,n2):
+    return n1-n2
 
+def multiply(n1,n2):
+    return n1*n2
+
+def divide(n1,n2):
+    return n1/n2
+
+dict_operations={"+":add,
+                "-":subtract,
+                "*":multiply,
+                 "/":divide
+                 }
+
+print(dict_operations["*"](2,3))
+while 5>4:
+    keep_playing=True
+    first_number= int(input("first number:"))
+    math_op= input("math operator from +,-,*,/")
+    second_number=int(input("second number:"))
+
+    value=dict_operations[math_op](first_number,second_number)
+    print(value)
+    while keep_playing==True:
+        print(keep_playing)
+        keep_playing_question=input("keep playing?:")
+        if keep_playing_question=="yes":
+            first_number=value
+            math_op = input("math operator from +,-,*,/")
+            second_number = int(input("second number:"))
+            value = dict_operations[math_op](first_number, second_number)
+        elif keep_playing_question=="no":
+            keep_playing=False
+        print(value)
--- Day 10/Multiple Return Values/task.py
+++ Day 10/Multiple Return Values/task.py
@@ -1,4 +1,4 @@
-def format_name(f_name, l_name):
+ def format_name(f_name, l_name):
     formated_f_name = f_name.title()
     formated_l_name = l_name.title()
     return f"{formated_f_name} {formated_l_name}"
